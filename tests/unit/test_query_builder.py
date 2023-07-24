f62de9ca14f73469cf5572c73ee1dc2be0ec8f39
Dilip Kumar Sharma <diliplakshya@gmail.com>
2023-07-24 19:41:09 +0530

    Add some unit tests for query_builder.py

---
+++ tests/unit/test_query_builder.py
@@ -0,0 +1,85 @@
+import os
+import csv
+import pytest
+
+from mail_helper import MailHelper
+from src.rule_engine.query_builder import QueryBuilder
+
+
+test_schema_dir = "tests/sample_schema"
+
+# Test 1
+table_column_info = os.path.join(test_schema_dir, "table_column_info.csv")
+
+
+def read_table_column_info(file_name):
+    with open(file_name, newline="") as csvfile:
+        data = csv.reader(csvfile, delimiter=",")
+        next(data)  # skip header row
+
+        return [[int(row[0]), row[1], row[2]] for row in data if row]
+
+
+@pytest.mark.parametrize(
+    "condition_code, expected_table_name, expected_column_name",
+    read_table_column_info(table_column_info),
+)
+def test_get_table_info(condition_code, expected_table_name, expected_column_name):
+    query_builder = QueryBuilder()
+
+    # Get table info for From field
+    table, column = query_builder.get_table_info(condition_code)
+
+    assert table == expected_table_name
+
+    assert column == expected_column_name
+
+
+# Test 2
+match_query_string = os.path.join(test_schema_dir, "match_query_string.csv")
+
+
+def read_match_query_string(file_name):
+    with open(file_name, newline="") as csvfile:
+        data = csv.reader(csvfile, delimiter=",")
+        next(data)  # skip header row
+        print(data)
+        return [[row[0], row[1], row[2]] for row in data if row]
+
+
+@pytest.mark.parametrize(
+    "column, predicate_value, expected_query",
+    read_match_query_string(match_query_string),
+)
+def test_gen_match_query_str(column, predicate_value, expected_query):
+    query_builder = QueryBuilder()
+
+    # Get table info for From field
+    query_string = query_builder.gen_match_query_str(column, predicate_value)
+
+    assert query_string == expected_query
+
+
+# Test 3
+date_duration = os.path.join(test_schema_dir, "date_duration.csv")
+
+
+def read_date_duration(file_name):
+    with open(file_name, newline="") as csvfile:
+        data = csv.reader(csvfile, delimiter=",")
+        next(data)  # skip header row
+        print(data)
+        return [[int(row[0]), row[1], int(row[2])] for row in data if row]
+
+
+@pytest.mark.parametrize(
+    "predicate_value, predicate_duration, date_duration",
+    read_date_duration(date_duration),
+)
+def test_get_date_duration(predicate_value, predicate_duration, date_duration):
+    query_builder = QueryBuilder()
+
+    # Get table info for From field
+    duration = query_builder.get_date_duration(predicate_value, predicate_duration)
+
+    assert duration == date_duration
